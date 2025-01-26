ace2edb13fdfa04ccdf78a413e5e88bd0a87b9c9
crideocdev <crideocdev@gmail.com>
2025-01-26 19:41:28 +0100

    Creacion de todas las ventanas a usar

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <></>
+    </NavigationContainer>
+  )
+}
+
+export default App
